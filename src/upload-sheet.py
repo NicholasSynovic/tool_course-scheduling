e8f801c6e4f1f299d8dce71a3d01eee19ed6a92c
acgrigores <acgrigores@gmail.com>
2024-07-01 18:41:07 +0000

    fixed upload-sheet

--- src/upload-sheet.py
+++ src/upload-sheet.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import os
 
 st.title("Excel File Uploader")
 
@@ -8,3 +9,6 @@ uploaded_file = st.file_uploader("Choose an Excel file", type=["xlsx"])
 #confirms file has been uploaded
 if uploaded_file is not None:
     st.write(":sob:")
+    save_path = os.path.join("src/", uploaded_file.name)
+    with open(save_path, "wb") as f:
+        f.write(uploaded_file.getbuffer())
