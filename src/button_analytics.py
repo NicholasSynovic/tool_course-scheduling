9082f2691649a9ebbfd43b24e2f4ae6cc19c2f9f
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-07-01 11:54:36 -0500

    Add buttons

---
+++ src/button_analytics.py
@@ -0,0 +1,29 @@
+# Import Streamlit
+import streamlit as st
+
+"""
+Buttons:
+
+- Course Schedule
+- Online only courses
+- Schedule Density
+- Course Enrollment Health
+- Instructor Assignments
+- Number of Assignments per Faculty
+- Course by Number
+- Teaching Distribution by Weighted Enrollment
+- Enrollments by Course Level
+"""
+
+# Title of your app
+st.title("Button Demos")
+
+st.button(label="Course Schedule")
+st.button(label="Online Only Courses")
+st.button(label="Schedule Density")
+st.button(label="Course Enrollment Health")
+st.button(label="Instructor Assignments")
+st.button(label="Number of Assignments Per Faculty")
+st.button(label="Course by Number")
+st.button(label="Teaching Distribution by Weighted Enrollment")
+st.button(label="Enrollments by Course Level")
