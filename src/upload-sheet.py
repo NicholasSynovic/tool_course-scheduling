2ad65c88f657903df9d1e950bb2a4d8d30caf81d
acgrigores <acgrigores@gmail.com>
2024-07-01 19:25:11 +0000

    uploads a .db file to the backend

--- src/upload-sheet.py
+++ src/upload-sheet.py
@@ -1,14 +1,31 @@
-import streamlit as st
+import sqlite3
+
 import pandas as pd
-import os
+import streamlit as st
+from streamlit.runtime.uploaded_file_manager import UploadedFile
 
 st.title("Excel File Uploader")
 
-uploaded_file = st.file_uploader("Choose an Excel file", type=["xlsx"])
+uploaded_file: UploadedFile = st.file_uploader(
+    "Choose an Excel file", type=["xlsx"]
+)
 
-#confirms file has been uploaded
+# runs if file has been uploaded
 if uploaded_file is not None:
-    st.write(":sob:")
-    save_path = os.path.join("src/", uploaded_file.name)
-    with open(save_path, "wb") as f:
-        f.write(uploaded_file.getbuffer())
+    # saves file to backend
+    # save_path = os.path.join("src/", "excelcoursedb.xlsx")
+    # with open(save_path, "wb") as f:
+    #     f.write(uploaded_file.getbuffer())
+    df = pd.read_excel(io=uploaded_file)
+
+    # converts excel to .db file (and saves?)
+    conn = sqlite3.connect(database=":memory:")
+
+    table = "schedule"
+    df.to_sql(table, conn, if_exists="replace", index=False)
+
+    print(df)
+
+    conn.close()
+
+    st.markdown("Hello world")
