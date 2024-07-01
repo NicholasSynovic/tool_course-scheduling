3b2dfac9f8fa00b028f639b77e05d0d4612705ce
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-07-01 11:56:26 -0500

    Add buttons with help messages on hover

--- src/button_analytics.py
+++ src/button_analytics.py
@@ -4,12 +4,39 @@ import streamlit as st
 # Title of your app
 st.title("Button Demos")
 
-st.button(label="Course Schedule")
-st.button(label="Online Only Courses")
-st.button(label="Schedule Density")
-st.button(label="Course Enrollment Health")
-st.button(label="Instructor Assignments")
-st.button(label="Number of Assignments Per Faculty")
-st.button(label="Course by Number")
-st.button(label="Teaching Distribution by Weighted Enrollment")
-st.button(label="Enrollments by Course Level")
+st.button(
+    label="Course Schedule",
+    help="Hello world",
+)
+st.button(
+    label="Online Only Courses",
+    help="Hello world",
+)
+st.button(
+    label="Schedule Density",
+    help="Hello world",
+)
+st.button(
+    label="Course Enrollment Health",
+    help="Hello world",
+)
+st.button(
+    label="Instructor Assignments",
+    help="Hello world",
+)
+st.button(
+    label="Number of Assignments Per Faculty",
+    help="Hello world",
+)
+st.button(
+    label="Course by Number",
+    help="Hello world",
+)
+st.button(
+    label="Teaching Distribution by Weighted Enrollment",
+    help="Hello world",
+)
+st.button(
+    label="Enrollments by Course Level",
+    help="Hello world",
+)
