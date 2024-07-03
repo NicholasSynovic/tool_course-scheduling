11f914275d7afc48d8b623828a002f9970eb3274
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-07-03 13:38:28 -0500

    Add support for additional metadata

--- proj/main.py
+++ proj/main.py
@@ -57,7 +57,6 @@ def main() -> None:
         with column1:
             streamlit.button(
                 label="Show Course Schedule",
-                help="Show the current course schedule",
                 use_container_width=True,
                 on_click=CourseSchedule(
                     conn=streamlit.session_state["dbConn"]
@@ -65,7 +64,6 @@ def main() -> None:
             )
             streamlit.button(
                 label="Online Only Courses",
-                help="Hello world",
                 use_container_width=True,
                 on_click=OnlineCourseSchedule(
                     conn=streamlit.session_state["dbConn"]
@@ -73,7 +71,6 @@ def main() -> None:
             )
             streamlit.button(
                 label="Schedule Density",
-                help="Hello world",
                 use_container_width=True,
                 on_click=ScheduleDensity(
                     conn=streamlit.session_state["dbConn"]
@@ -82,19 +79,16 @@ def main() -> None:
             # TODO: Implement viz for this
             streamlit.button(
                 label="Course Enrollment Health",
-                help="Hello world",
                 use_container_width=True,
             )
             streamlit.button(
                 label="Instructor Assignments",
-                help="Hello world",
                 use_container_width=True,
             )
 
         with column2:
             streamlit.button(
                 label="Number of Assignments Per Faculty Member",
-                help="Hello world",
                 use_container_width=True,
                 on_click=AssignmentsPerFaculty(
                     conn=streamlit.session_state["dbConn"],
@@ -102,30 +96,43 @@ def main() -> None:
             )
             streamlit.button(
                 label="Course by Number",
-                help="Hello world",
                 use_container_width=True,
             )
             streamlit.button(
                 label="Teaching Distribution by Weighted Enrollment",
-                help="Hello world",
                 use_container_width=True,
             )
             streamlit.button(
                 label="Enrollments by Course Level",
-                help="Hello world",
                 use_container_width=True,
             )
             streamlit.button(
                 label="In Trouble Courses",
-                help="Hello world",
                 use_container_width=True,
             )
 
         streamlit.divider()
 
+        if streamlit.session_state["analyticTitle"] is not None:
+            streamlit.markdown(
+                body=f"## {streamlit.session_state['analyticTitle']}"
+            )
+
+        if streamlit.session_state["analyticSubtitle"] is not None:
+            streamlit.markdown(
+                body=f">  {streamlit.session_state['analyticSubtitle']}"
+            )
+
         try:
             fig: Figure
             for fig in streamlit.session_state["figList"]:
+
+                if streamlit.session_state["figListTitles"] is not None:
+                    streamlit.markdown(
+                        body=f"### \
+                        {streamlit.session_state['figListTitles'].pop(0)}"
+                    )
+
                 streamlit.plotly_chart(
                     figure_or_data=fig,
                     use_container_width=True,
@@ -136,6 +143,13 @@ def main() -> None:
         try:
             df: DataFrame
             for df in streamlit.session_state["dfList"]:
+
+                if streamlit.session_state["dfListTitles"] is not None:
+                    streamlit.markdown(
+                        body=f"### \
+                        {streamlit.session_state['dfListTitles'].pop(0)}"
+                    )
+
                 streamlit.dataframe(
                     data=df,
                     use_container_width=True,
