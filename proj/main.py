c2ae2c1d55c57ff6b44e8e3cd9b543770864504d
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-07-03 14:09:42 -0500

    Fix spacing error

--- proj/main.py
+++ proj/main.py
@@ -111,7 +111,7 @@ def main() -> None:
 
         if streamlit.session_state["analyticSubtitle"] is not None:
             streamlit.markdown(
-                body=f">  {streamlit.session_state['analyticSubtitle']}"
+                body=f"> {streamlit.session_state['analyticSubtitle']}"
             )
 
         try:
