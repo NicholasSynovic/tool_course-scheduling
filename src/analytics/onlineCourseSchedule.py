c8fc721a801dc412d560e8e62d515f50fe906a6f
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-07-07 16:51:11 -0500

    Add docstring

--- src/analytics/onlineCourseSchedule.py
+++ src/analytics/onlineCourseSchedule.py
@@ -6,82 +6,41 @@ from pandas import DataFrame
 
 from src.analytics.courseSchedule import CourseSchedule
 from src.utils import clearContent
+from src.utils.analytic import Analytic
 
 
-class OnlineCourseSchedule:
+class OnlineCourseSchedule(Analytic):
     """
-    Class to retrieve and manage online course schedule data.
+    OnlineCourseSchedule class to retrieve and manage online course schedule
+    data.
 
-    Attributes
-    ----------
-    conn : Connection
-        A database connection object.
-
-    Methods
-    -------
-    __init__(conn: Connection) -> None:
-        Initialize the class with a database connection.
-
-    get() -> pandas.DataFrame:
-        Retrieve course schedule data filtered by 'ONLINE' facility and return as a DataFrame.
-
-    run() -> None:
-        Run the workflow to retrieve course schedule data and store it in Streamlit session state.
-    """  # noqa: E501
+    This class provides functionalities to compute and visualize the course
+    schedule data for online courses.
+    """
 
     def __init__(self, conn: Connection) -> None:
         """
-        Initialize the class with a database connection.
+        Initialize the OnlineCourseSchedule class with a database connection.
 
-        Parameters
-        ----------
-        conn : Connection
-            A database connection object.
+        This constructor sets up the database connection which will be used to
+        compute and visualize the online course schedule data.
 
-        Returns
-        -------
-        None
-
-        Notes
-        -----
-        This constructor initializes the class instance with a provided database connection object.
-
-        Examples
-        --------
-        >>> import sqlite3
-        >>> conn = sqlite3.connect(":memory:")
-        >>> example_instance = MyClass(conn)
-        [Initializes an instance of MyClass with a database connection]
-        """  # noqa: E501
-
+        :param conn: A database connection object.
+        :type conn: Connection
+        """
         self.conn: Connection = conn
 
     def compute(self) -> DataFrame:
         """
-        Retrieve course schedule data filtered by 'ONLINE' facility and return as a DataFrame.
+        Compute the course schedule data filtered by 'ONLINE' facility.
 
-        Parameters
-        ----------
-        None
+        This method fetches the course schedule data from the database and
+        filters it to include only the courses held online.
 
-        Returns
-        -------
-        pandas.DataFrame
-                A DataFrame containing course schedule data filtered by 'ONLINE' facility.
-
-        Notes
-        -----
-                This method performs the following steps:
-                        1. Retrieves course schedule data using the `get` method from the `CourseSchedule` class with the stored connection object.
-                        2. Filters rows where the 'FACILITY' column equals 'ONLINE'.
-                        3. Resets the index of the resulting DataFrame for clarity.
-
-        Examples
-        --------
-                >>> example_instance.get()
-                [Returns a DataFrame with course schedule data filtered by 'ONLINE' facility]
-        """  # noqa: E501
-
+        :return: A DataFrame containing course schedule data filtered by
+            'ONLINE' facility.
+        :rtype: DataFrame
+        """
         df: DataFrame = CourseSchedule(conn=self.conn).compute()
 
         df = df[df["FACILITY"] == "ONLINE"]
@@ -92,29 +51,16 @@ class OnlineCourseSchedule:
 
     def run(self) -> None:
         """
-        Run the workflow to retrieve course schedule data and store it in Streamlit session state.
+        Execute the workflow to retrieve and store online course schedule
+        data.
 
-        Parameters
-        ----------
-        None
+        This method computes the online course schedule data, clears existing
+        content, and updates the Streamlit session state with the resulting
+        data for visualization.
 
-        Returns
-        -------
-        None
-
-        Notes
-        -----
-        This method performs the following steps:
-            1. Initializes `streamlit.session_state['df']` and `streamlit.session_state['fig']` to None.
-            2. Retrieves course schedule data using the `get` method and assigns the result to `df`.
-            3. Stores the retrieved DataFrame `df` in `streamlit.session_state` for use in the Streamlit app.
-
-        Examples
-        --------
-        >>> example_instance.run()
-        [Runs the workflow to retrieve and store course schedule data in Streamlit session state.]
-        """  # noqa: E501
-
+        :return: None
+        :rtype: None
+        """
         clearContent()
 
         dfs: List[DataFrame] = [self.compute()]
