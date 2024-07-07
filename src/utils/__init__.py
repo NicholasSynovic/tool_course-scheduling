dd4bf33bcc06d12109b33e33ee09c1d5f6850751
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-07-07 17:05:22 -0500

    Add docstring

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -18,24 +18,30 @@ SESSION_STATE_KEYS: List[str] = [
 
 def datetimeToMinutes(dt: datetime) -> int:
     """
-    Converts a datetime object to the total number of minutes since midnight.
+    Convert a datetime object to the total number of minutes since midnight.
 
-    Parameters:
-    ----------
-    dt : datetime
-        A datetime object representing the time of day.
+    This function takes a datetime object and converts it into the total
+    number of minutes since midnight.
 
-    Returns:
-    -------
-    int
-        The total number of minutes since midnight represented by the datetime object.
-
-    """  # noqa: E501
-
+    :param dt: The datetime object to be converted.
+    :type dt: datetime
+    :return: The total number of minutes since midnight.
+    :rtype: int
+    """
     return dt.hour * 60 + dt.minute
 
 
 def initialState() -> None:
+    """
+    Initialize the session state keys to None if they do not exist.
+
+    This function checks if the keys defined in SESSION_STATE_KEYS exist in
+    the Streamlit session state. If they do not exist, it initializes them to
+    None.
+
+    :return: None
+    :rtype: None
+    """
     key: str
     for key in SESSION_STATE_KEYS:
         if key not in streamlit.session_state:
@@ -43,12 +49,30 @@ def initialState() -> None:
 
 
 def resetState() -> None:
+    """
+    Reset the session state keys to None.
+
+    This function sets all the keys defined in SESSION_STATE_KEYS to None in
+    the Streamlit session state.
+
+    :return: None
+    :rtype: None
+    """
     key: str
     for key in SESSION_STATE_KEYS:
         streamlit.session_state[key] = None
 
 
 def clearContent() -> None:
+    """
+    Clear the content-related session state keys.
+
+    This function sets the content-related keys (from the third key onwards in
+    SESSION_STATE_KEYS) to None in the Streamlit session state.
+
+    :return: None
+    :rtype: None
+    """
     key: str
     for key in SESSION_STATE_KEYS[2::]:
         streamlit.session_state[key] = None
