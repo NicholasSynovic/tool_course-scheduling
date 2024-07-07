3b73af96d4b0ad958c8cc4427a2025bd58d0a0d6
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-07-07 17:03:16 -0500

    Add docstring

--- src/analytics/teachingDistributionByWeightedEnrollment.py
+++ src/analytics/teachingDistributionByWeightedEnrollment.py
@@ -7,13 +7,44 @@ from plotly.graph_objects import Figure
 
 from src.analytics.courseSchedule import CourseSchedule
 from src.utils import clearContent
+from src.utils.analytic import Analytic
 
 
-class TeachingDistributionByWeightedEnrollment:
+class TeachingDistributionByWeightedEnrollment(Analytic):
+    """
+    TeachingDistributionByWeightedEnrollment class to compute and visualize
+    the distribution of teaching assignments based on weighted enrollment.
+
+    This class provides functionalities to compute and plot the total weighted
+    enrollment per instructor.
+    """
+
     def __init__(self, conn: Connection) -> None:
+        """
+        Initialize the TeachingDistributionByWeightedEnrollment class with a
+        database connection.
+
+        This constructor sets up the database connection which will be used to
+        compute and visualize the teaching distribution by weighted
+        enrollment.
+
+        :param conn: A database connection object.
+        :type conn: Connection
+        """
         self.conn: Connection = conn
 
     def compute(self) -> Series:
+        """
+        Compute the total weighted enrollment per instructor.
+
+        This method fetches the course schedule data from the database,
+        computes the total weighted enrollment per instructor, and sorts the
+        results in descending order.
+
+        :return: A Series containing the total weighted enrollment per
+            instructor.
+        :rtype: Series
+        """
         df: DataFrame = CourseSchedule(conn=self.conn).compute()
 
         data: Series = (
@@ -25,6 +56,19 @@ class TeachingDistributionByWeightedEnrollment:
         return data.sort_values(by="WEIGHTED_ENROLL_TOTAL", ascending=False)
 
     def plot(self, data: Series) -> Figure:
+        """
+        Plot the total weighted enrollment per instructor.
+
+        This method creates a horizontal bar chart to visualize the total
+        weighted enrollment per instructor.
+
+        :param data: A Series containing the total weighted enrollment per
+            instructor.
+        :type data: Series
+        :return: A Plotly Figure representing the total weighted enrollment
+            per instructor.
+        :rtype: Figure
+        """
         fig: Figure = px.bar(
             data,
             x="WEIGHTED_ENROLL_TOTAL",
@@ -51,6 +95,19 @@ class TeachingDistributionByWeightedEnrollment:
         return fig
 
     def run(self) -> None:
+        """
+        Execute the workflow to compute and visualize the teaching
+            distribution by weighted enrollment.
+
+        This method performs the following steps:
+        1. Clears existing content.
+        2. Computes the total weighted enrollment per instructor.
+        3. Updates the Streamlit session state with the resulting figure for
+            visualization.
+
+        :return: None
+        :rtype: None
+        """
         clearContent()
 
         streamlit.session_state["analyticTitle"] = (
