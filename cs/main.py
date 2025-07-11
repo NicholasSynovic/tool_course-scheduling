6b601fc618b7d67c07206e2a11dd8b7eff2809df
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2025-07-11 11:33:54 -0500

    Add command line runner

--- cs/main.py
+++ cs/main.py
@@ -1,7 +1,11 @@
-if __name__ == "__main__":
+def main() -> None:
     print("""Loyola University Chicago (LUC) Course Scheduling (CS) Utility
 
 To run, type the following and press ENTER in your terminal:
 
 `streamlit run cs/app.py`
 """)  # noqa: T201
+
+
+if __name__ == "__main__":
+    main()
